7d78cfcd3c6bc3710d220ba9554caf3a9cb9c079
Software Developer <pcloudcombo@gmail.com>
2025-02-27 05:39:08 -0600

    update

--- scripts/preload.js
+++ scripts/preload.js
@@ -1,47 +1,50 @@
 import Handlers from './handlers.js';
 
 function check(data, imageContainer, osdContainer) {
-  if (!data?.urls) return;
-  let index = data?.index ? Number(data.index) : 0;
-  process2(data.urls[0], imageContainer, osdContainer);
-  const params = new URLSearchParams(location.search);
-  if (params.has('url')) process(params.get('url'));
-  else if (params.has('urls')) process(decode(params.get('urls')));
-  load(imageContainer, osdContainer);
-  open(imageContainer);
+  try {
+    if (!data?.urls) throw 'No Urls array passed via postmessage';
+    let index = data?.index ? Number(data.index) : 0;
+    let transfer = process(data.urls);
+    load(transfer, imageContainer, osdContainer);
+    open(index, imageContainer);
+  } catch (error) {
+    alert(error);
+  }
 }
 
 function process(urls) {
-  document.getElementById('urls-textarea').value = urls;
-}
-
-function process2(dataUrl, imageContainer, osdContainer) {
-  const mimeType = dataUrl.split(';')[0].split(':')[1];
-  const byteCharacters = atob(dataUrl.split(',')[1]);
-  const byteArrays = []
-  for (let offset = 0; offset < byteCharacters.length; offset++) {
-    byteArrays.push(byteCharacters.charCodeAt(offset));
+  if (!Array.isArray(urls)) throw 'Urls is not an array at process function';
+  let transfer = new DataTransfer();
+  for (const url of urls) {
+    let file = extract(url);
+    transfer.items.add(file);
   }
-  const byteArray = new Uint8Array(byteArrays);
-  const blob = new Blob([byteArray], { type: mimeType });
-  const file = new File([blob], 'image.png', { type: mimeType });
-  const dataTransfer = new DataTransfer();
-  dataTransfer.items.add(file);
-  const fileInput = document.getElementById('input');
-  fileInput.files = dataTransfer.files;
-  Handlers.handleFiles({ currentTarget: fileInput }, imageContainer, osdContainer)
+  return transfer;
 }
 
-function load(imageContainer, osdContainer) {
-  Handlers.URLs.submit(imageContainer, osdContainer)
+function extract(url) {
+  let mime = url.split(';')[0].split(':')[1];
+  let chars = atob(url.split(',')[1]);
+  let bytes = []
+  for (let offset = 0; offset < chars.length; offset++) {
+    bytes.push(chars.charCodeAt(offset));
+  }
+  let byteArray = new Uint8Array(bytes);
+  let blob = new Blob([byteArray], { type: mime });
+  let file = new File([blob], 'image.png', { type: mime });
+  return file;
 }
 
-function decode(str) {
-
+function load(transfer, imageContainer, osdContainer) {
+  let input = document.getElementById('input');
+  input.files = transfer.files;
+  Handlers.handleFiles({ currentTarget: input }, imageContainer, osdContainer);
 }
 
-function open(imageContainer) {
-  imageContainer.querySelector('img')?.click();
+function open(index, imageContainer) {
+  let imgs = imageContainer.querySelectorAll('img');
+  if (imgs.length < index + 1) throw 'Index higher than total images'
+  imgs[index].click();
 }
 
 export default {
